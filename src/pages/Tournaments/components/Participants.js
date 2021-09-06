0ba48f03347a594655ab02d02ad21ce81981ac30
Sergio Alcala <checo_2k2@hotmail.com>
2021-09-06 16:31:46 -0500

    (bugfix) show participants when torunament is a cup

--- src/pages/Tournaments/components/Participants.js
+++ src/pages/Tournaments/components/Participants.js
@@ -6,14 +6,32 @@ import useTournament from '../../../hooks/useTournament';
 
 
 const Participants = ({id}) => {  
-  const { participants, deleteParticipant } = useTournament(id);
+  const { participants,participantsCup, detail:{kind}, deleteParticipant } = useTournament(id);
   const handleDelete = (user) => {  
     deleteParticipant(user);
   }
-
+  
   return (
     <List>
-      {participants.map( participant => (
+      { kind === 'liga' && participants.map( participant => (
+        <ListItem key={participant.id}>
+          <ListItemAvatar>
+            <Avatar alt={participant.pokemonNick} src={participant.photoURL} />
+          </ListItemAvatar>
+          <ListItemText
+            primary={participant.pokemonNick}
+          />
+          <ListItemSecondaryAction>
+            <IconButton 
+              onClick={ () => handleDelete(participant) }
+              edge="end" 
+              aria-label="eliminar">
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      ) )}
+      { kind === 'copa' && participantsCup.map( participant => (
         <ListItem key={participant.id}>
           <ListItemAvatar>
             <Avatar alt={participant.pokemonNick} src={participant.photoURL} />
